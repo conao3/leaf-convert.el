55f07fbdd437a8c0534b3f13ee76188a23788814
conao3 <conao3@gmail.com>
2020-04-14 23:01:32 +0900

    tagged v1.1.1

--- leaf-convert.el
+++ leaf-convert.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2020  Naoya Yamashita
 
 ;; Author: Naoya Yamashita <conao3@gmail.com>
-;; Version: 1.1.0
+;; Version: 1.1.1
 ;; Keywords: tools
 ;; Package-Requires: ((emacs "26.1") (leaf "3.6.0") (leaf-keywords "1.1.0") (ppp "2.1"))
 ;; URL: https://github.com/conao3/leaf-convert.el
